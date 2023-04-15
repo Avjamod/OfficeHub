5194913c41ae16dea39fb87515941bf54d4a8178
arpan jamod <jamodarpan99@gmail.com>
2023-04-15 12:51:54 +0530

    set project repo

---
+++ routes/connection.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const MONGO_URL = 'mongodb+srv://vatsaljajadiya1310:8iII8939646@cluster0.oahfaag.mongodb.net/officehub';
+
+mongoose.set("strictQuery", false);
+
+
+const serv = mongoose.connect(MONGO_URL)
+    .then(console.log(`DB Connection is success!`))
+    .catch((error) => {
+        console.log(`DB connection is Failed!`)
+        console.log(error);
+});
+
+module.exports = serv;    
